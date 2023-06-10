1c06c9e6ebfc94e114538532987eab8011ff9ed0
phatlino <phatlongtoan@gmail.com>
2023-06-11 00:21:57 +0700

    update to pull

--- ThiTracNghiem/BLL/DeThi.cs
+++ ThiTracNghiem/BLL/DeThi.cs
@@ -1,4 +1,4 @@
-﻿using QLShopThoiTrang.DAL;
+﻿using ThiTracNghiem.DAL;
 using System;
 using System.Collections.Generic;
 using System.Data;
--- ThiTracNghiem/DAL/DBConfig.cs
+++ ThiTracNghiem/DAL/DBConfig.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 
-namespace QLShopThoiTrang.DAL
+namespace ThiTracNghiem.DAL
 {
     class DBConfig
     {
